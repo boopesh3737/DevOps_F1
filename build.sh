be6497956b8117d21e81251538477c9e9a6c9805
boopesh3737 <127740144+boopesh3737@users.noreply.github.com>
2025-04-03 16:21:24 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 docker build -t task37 .
 echo Hyyy
 docker login -u boopeshs -p 02-Apr-05
-docker tag task37 boopeshs/task_f
-docker push boopeshs/task_f
+docker tag task37 boopeshs/final_task
+docker push boopeshs/final_task
 kubectl apply -f  deploy.yaml --validate=false
 kubectl apply -f svc.yaml --validate=false
