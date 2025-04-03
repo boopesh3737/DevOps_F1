467cf9cf7923f4311ff8cff13fb7c9541f9f80b4
boopesh3737 <127740144+boopesh3737@users.noreply.github.com>
2025-04-03 16:07:53 +0530

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-    echo hyyy
-    sh 'chmod +x build.sh'
-    sh './build.sh'
-    docker login -u boopeshs -p 02-Apr-05
-    docker tag test boopeshs/task_f
-    docker push boopeshs/task_f
-    
